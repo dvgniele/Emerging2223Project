6cfe2c0ed3c38da7845b8649e9f3ee82a46b4ead
dvgniele <ktm17@live.it>
2023-06-13 17:28:37 +0200

    add: render, ambient

--- ambient.erl
+++ ambient.erl
@@ -3,8 +3,11 @@
 -import(grid, [create_grid/2, get_cell/3, set_cell/4]).
 
 init_world() ->
+    register(ambient, self()),
     N = 50,
     M = 50,
     Grid = create_grid(N, M),
-    get_cell(Grid, 0,0)
+    world_listener(Grid)
     .
+
+world_listener(Grid) -> .
