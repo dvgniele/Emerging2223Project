de0afb9ba7fb6bd40ce2fff60e813177ba9a44b4
dvgniele <ktm17@live.it>
2023-06-13 17:29:03 +0200

    add: wellknown actor

---
+++ wellknown.erl
@@ -0,0 +1,26 @@
+-module(wellknown).
+-export().
+
+wellknown() ->
+    register(wellknown, self())
+    io:format("##    Wellknown: actor registered as 'wellknown' with pid ~p    ##\n", [self()])
+    wellknown([])
+
+wellknown(PIDLIST) -> 
+    receive
+        {getFriends, PID1, PID2, Ref} ->
+            % invio della lista di amici al processo di friendship che ha effettuato la richiesta
+            PID1 ! {myFriends, PIDLIST, Ref},
+
+            % controllo della presenza di {PID1, PID2} in PIDLIST
+            % se non è contenuto, lo aggiunge
+            case lists:member({PID1, PID2}, PIDLIST) of
+                true -> wellknown(PIDLIST);
+                false -> 
+                    monitor(process, PID1),
+                    io:format("##    Wellknown: adding new frenship for pid: ~p    ##\n", [PID1]),
+                    % viene aggiunto successivamente alla restituzione della lista di friendship "myFriends"
+                    % dato che non ha senso avere se stessi nella lista di amici
+                    wellknown([{PID1, PID2} | PIDLIST])
+            end;
+    end.
