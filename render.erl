344712255c95eca785dd708b027bfd4b2a47b258
ToriPendi <francesco.santilli11@gmail.com>
2023-06-16 17:43:05 +0200

    backbone of render

--- render.erl
+++ render.erl
@@ -1,1 +1,29 @@
 -module(render).
+-import(grid,[create_grid/2]).
+-export([render/2]).
+
+render_loop() ->
+    sleep(1000),
+    render ! {draw},
+    render_loop().
+
+render(W, H) ->
+    register(render, self()),
+    io:format("##    Render: actor registered as 'render' with pid ~p    ##\n", [self()])
+    spawn_link(?MODULE, render_loop, []),
+    Grid = create_grid(W,H),
+    render(Grid, W, H).
+
+render(CarsPositionsMap, W, H) ->
+    
+    receive
+        {draw} -> 
+            io:format("weooofraaa"),
+            render(CarsPositionsMap, W, H)
+    end.
+
+% Stampa lo stato attuale della grid
+draw() -> .
+
+% Sospende l'esecuzione per N ms
+sleep(N) -> receive after N -> ok end.
