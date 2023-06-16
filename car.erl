848fc767ae2928574efd931d449ec9ae4e1877f1
ToriPendi <francesco.santilli11@gmail.com>
2023-06-16 17:41:16 +0200

    added state/2 and move

--- car.erl
+++ car.erl
@@ -1,5 +1,6 @@
 -module(car).
 -export().
+-import(grid,[create_grid/2]).
 
 main() -> 
     Pid_frindship = spawn_link(?MODULE, friendship, []),
@@ -9,7 +10,8 @@ main() ->
 friendship(StatePid, FRIENDLIST) when FRIENDLIST =:= [] -> 
     link(StatePid),
     Ref = make_ref(),
-
+    % invia a StatePID il PID della friendship
+    StatePid ! {friendshipPid, self()},
     % init della lista di amici richiedendola a wellknown
     wellknown ! {getFriends, self(), StatePid, Ref},
     receive
@@ -77,6 +79,65 @@ get_friend(StatePid, PIDLIST, NewFriends) - >
 
 
 
-state() -> .
+state({X,Y},{R,C}) -> 
+    Grid = create_grid(R,C),
+    receive
+        {friendshipPid, PID} -> state(Grid,{X,Y},PID)
+    end.
+
+state(Grid, {X,Y}, FriendshipPid) -> 
+    receive
+        %%TODO
+    end.
 
 detect() -> .
+
+move(StatePid, W, H, {Target_X, Target_Y}) -> 
+    Ref = make_ref(),
+    StatePid ! {getMyPosition, self(), Ref},
+    receive 
+        {myPosition, {X, Y}, Ref} ->
+            case {X =:= Target_X, Y =:= Target_Y} of
+                {true,true} ->
+                    {X,Y};
+
+                {true,false} ->
+                    {X, (Y + moveY(Y, Target_Y, H)) rem (H)}
+
+                {false,true} ->
+                    {(X + moveX(X, Target_X, W)) rem (W), Y}
+
+                {false,false} ->
+                           Axis = rand:uniform(2),
+                            case Axis of 
+                                1 -> {(X + moveX(X, Target_X, W)) rem (W), Y};
+                                2 -> {X, (Y + moveY(Y, Target_Y, H)) rem (H)}
+                            end
+            
+            end
+    end.
+
+moveX(X, Target_X, W) ->
+        Margin_R = abs(Target_X - ((X+1) rem W)), 
+        Margin_L = abs(Target_X - ((X-1) rem W)),
+
+        io:format("X: Margin_R: ~p, Margin_L: ~p~n", [Margin_R, Margin_L]),
+
+        case Margin_R =< Margin_L of
+            true -> 1;
+            false -> -1
+        end.
+
+moveY(Y, Target_Y, H) ->
+        Margin_T = abs(Target_Y - ((Y+1) rem H)), 
+        Margin_B = abs(Target_Y - ((Y-1) rem H)),
+
+        io:format("Y: Margin_T: ~p, Margin_B: ~p~n", [Margin_T, Margin_B]),
+
+        case Margin_T =< Margin_B of
+            true -> 1;
+            false -> -1
+        end.
+
+% Sospende l'esecuzione per N ms.
+sleep(N) -> receive after N -> ok end.
