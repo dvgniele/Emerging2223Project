e19d9d280b09cc15e08ef57269455cf491ba2566
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-06-19 16:23:31 +0200

    init ambient

--- ambient.erl
+++ ambient.erl
@@ -1,13 +1,52 @@
 -module(ambient).
+-import(grid, [create_grid/2, get_cell/3, set_cell/4, update_position/6]).
 -export([init_world/0]).
--import(grid, [create_grid/2, get_cell/3, set_cell/4]).
 
 init_world() ->
     register(ambient, self()),
-    N = 50,
-    M = 50,
-    Grid = create_grid(N, M),
-    world_listener(Grid)
+    io:format("Ambient created with PID -> ~p\n", [self()]),
+    W = 50,
+    H = 50,
+    Grid = create_grid(W, H),
+    process_flag(trap_exit, true),
+    world_listener(Grid, W, H)
     .
 
-world_listener(Grid) -> .
+world_listener(Grid, W, H) ->
+        receive         
+        
+        {isFree, PID, X, Y, Ref} ->
+            NumberOfCars = get_cell(Grid, X, Y),
+            ListOfParkedCar = lists:filter(fun({_, IsFree}) -> IsFree =/= true end, NumberOfCars),
+            case length(ListOfParkedCar) of
+                0 -> PID ! {status, Ref, true};
+                1 -> PID ! {status, Ref, false}
+            end,
+            world_listener(Grid, W, H);
+
+        {park, PID, X, Y, _} -> 
+            NumberOfCars = get_cell(Grid, X, Y),
+            ListOfParkedCar = lists:filter(fun({_, IsFree}) -> IsFree =/= true end, NumberOfCars), % if the park is NOT avilable
+            case length(ListOfParkedCar) of
+                0 -> 
+                    MonitorRef = monitor(process, PID),
+                    UpdatedGrid = update_position(Grid, X, Y, {MonitorRef, false}, W, H),
+                    render ! {parked, PID, X, Y, true},
+                    world_listener(UpdatedGrid, W, H);
+                1 -> 
+                    io:format("The car ~p has been killed by ambiend because in {~p, ~p} there is already a car parked.\n", [PID, X, Y]),
+                    exit(PID, kill),
+                    world_listener(Grid, W, H)
+            end;
+
+        {'DOWN', Ref, process, Pid, Reason} ->
+            io:format("Ambient demonitoring car with PID -> ~p. Reason: ~p\n", [Pid, Reason]),
+            demonitor(Ref),
+            UpdatedGrid = maps:remove(Pid, Grid),
+            world_listener(UpdatedGrid, W, H);
+
+        {'EXIT', From, Reason} -> 
+            io:format("Ambient received exit message from ~p with reason ~p\n", [From, Reason]),
+            UpdatedGrid = maps:remove(From, Grid),
+            world_listener(UpdatedGrid, W, H)
+    end.
