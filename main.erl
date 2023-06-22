a0cb314071d005ab191d2774eec0e079c7b17da8
dvgniele <ktm17@live.it>
2023-06-22 16:39:16 +0200

    add: main module; add: compile and run script

--- main.erl
+++ main.erl
@@ -1,5 +1,39 @@
 -module(main).
 -export([init/0]).
--import(ambient, [init_world/0]).
+%-import(ambient, [init_world/2]).
+%-import(car, [main/4]).
 
-init() -> io:fwrite("hello, world\n").
+init() -> 
+    io:format("##    Main -> Starting    ##\n"),
+    W = 10, H = 10,
+    NCars = 10,
+    NCarsToPurge = 3,
+    SleepInterval = 10000,
+    spawn(ambient, init_world, [W, H]),
+    spawn(wellknown, wellknown, []),
+    spawn(render, render, [W, H]),
+    create_cars(NCars, [], W, H, SleepInterval, NCarsToPurge)
+    .
+
+create_cars(NCars, CarsPids, W, H, SleepInterval, NCarsToPurge) when NCars > 0 ->
+    io:format("##    Main -> Creating Cars\n"),
+    X = rand:uniform(W),
+    Y = rand:uniform(H),
+    CarPid = spawn(car, main, [X, Y, W, H]),
+    NewPids = [CarPid | CarsPids],
+    create_cars(NCars - 1, NewPids, W, H, SleepInterval, NCarsToPurge);
+
+
+create_cars(NCars, CarsPids, W, H, SleepInterval, NCarsToPurge) when NCars =:= 0 ->
+    io:format("##    Main -> Create_cars/6    ##\n"),
+    io:format("Cars List: ~p\n", [CarsPids]),
+    sleep(SleepInterval),
+    cars_purge().
+
+
+cars_purge() -> 
+    io:format("##    Cars Purge    ##").
+
+%%% sleep/1
+% Sospende l'esecuzione per N ms
+sleep(N) -> receive after N -> ok end.
---
+++ run.bat
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+erlc -o . ambient.erl
+erlc -o . car.erl
+erlc -o . grid.erl
+erlc -o . main.erl
+erlc -o . render.erl
+erlc -o . wellknown.erl
+
+PAUSE
+
+erl -sname eep -s main init
