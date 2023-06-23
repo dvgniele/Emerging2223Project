83946daa7ad17c8dfd9a8875a58b98cf80cfb6d9
Stefano Notari <stefano.notari2@studio.unibo.it>
2023-06-23 16:19:15 +0200

    added draw fro ambient grid

--- render.erl
+++ render.erl
@@ -27,7 +27,7 @@ render(Grid, W, H) ->
             render(NewGrid, W, H);
         % print new target for PID car
         {target, PID, X, Y} ->
-            io:format("Car ~p wants to park in {~p, ~p}\n", [PID, X, Y]),
+            %io:format("Car ~p wants to park in {~p, ~p}\n", [PID, X, Y]),
             render(Grid, W, H);
         % update the status of the car (parked or not)
         {parked, PID, X, Y, IsParked} -> 
@@ -36,33 +36,54 @@ render(Grid, W, H) ->
             render(NewCarsPositionsMap, W, H);
         % print the new friendlist
         {friends, PID, PIDLIST} ->
-            io:format("Car ~p new friend list:\n~p\n", [PID,PIDLIST]),
+            %io:format("Car ~p new friend list:\n~p\n", [PID,PIDLIST]),
             render(Grid, W, H);
         {'DOWN', Ref, process, Pid, _} ->
             demonitor(Ref),
             NewGrid = maps:remove(Pid, Grid),
             render(NewGrid, W, H);
         {draw} -> 
-            draw(Grid, W, H, 0, 0),
+            draw(Grid, W, H),
             render(Grid, W, H)
     end.
 
 % Stampa lo stato attuale della grid
 
-% print the current row
-draw(Grid, W, H, PosX, PosY) when PosY + 1 < H -> 
-    io:format("##    render -> draw/5    ##\n"),
-    CarsList = get_cell(Grid, PosX, PosY),
-    io:format("(~p  ~p) -> ~p\n", [PosX, PosY, length(CarsList)]),
+draw(Grid, W, H) ->
+    draw(Grid, W, H, 0, 0)
+    .
+
+draw(Grid, W, H, PosX, PosY) when PosY + 1 < H->
+    CurrentCars = get_cell(Grid, PosX, PosY),
+    case length(CurrentCars) of
+        0 -> io:format(" # ");
+        1 -> io:format(" X ");
+        _ -> io:format(" M ")
+    end,
     draw(Grid, W, H, PosX, PosY + 1);
 
-% increase the row and start to printing again from column 0
+% % increase the row and start to printing again from column 0
 draw(Grid, W, H, PosX, PosY) when PosX + 1 < W, PosY + 1 == H -> 
-    io:format("##    render -> draw/5 (2)   ##\n"),
-   draw(Grid, W, H, PosX + 1, 0);
+    io:format("\n"),
+    draw(Grid, W, H, PosX + 1, 0);
 
 draw(_, W, H, PosX, PosY) when PosX + 1 == W, PosY + 1 == H -> 
-    io:format("End draw\n", []).
+     io:format("\n\n\n Legenda: \n # -> no car \n X -> one car \n M -> many car \n", []).
+
+% % print the current row
+% draw(Grid, W, H, PosX, PosY) when PosY + 1 < H -> 
+%     io:format("##    render -> draw/5    ##\n"),
+%     CarsList = get_cell(Grid, PosX, PosY),
+%     io:format("(~p  ~p) -> ~p\n", [PosX, PosY, length(CarsList)]),
+%     draw(Grid, W, H, PosX, PosY + 1);
+
+% % increase the row and start to printing again from column 0
+% draw(Grid, W, H, PosX, PosY) when PosX + 1 < W, PosY + 1 == H -> 
+%     io:format("##    render -> draw/5 (2)   ##\n"),
+%    draw(Grid, W, H, PosX + 1, 0);
+
+% draw(_, W, H, PosX, PosY) when PosX + 1 == W, PosY + 1 == H -> 
+%     io:format("End draw\n", []).
 
 % Sospende l'esecuzione per N ms
 sleep(N) -> receive after N -> ok end.
